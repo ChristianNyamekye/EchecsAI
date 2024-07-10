0cc041ba4d59e778e2bbd8be56c24cbfb9a5743b
Christian Nyamekye <christiankingnyamekye@gmail.com>
2024-07-10 01:05:53 -0400

    package.json and gitignore

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
+
+// import { defineConfig } from 'next';
+
+// export default defineConfig({
+//   experimental: {
+//     optimizePackageImports: ['@mantine/core', '@mantine/hooks']
+//   }
+// });
