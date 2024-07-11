2cd9f80065a569b5986b86d5e85c649eba9fdd69
Christian Nyamekye <christiankingnyamekye@gmail.com>
2024-07-10 20:11:59 -0400

    gcloud config

--- src/pages/api/best-move.js
+++ src/pages/api/best-move.js
@@ -1,3 +1,40 @@
+// import { Chess, validateFen } from "chess.js";
+// import { getBestMove } from "../../../lib/stockfish";
+// import { analyzePosition } from "../../../lib/stockfish";
+// import { getFen } from "../../../lib/chess";
+
+// export default async (req, res) => {
+//   if (req.method === "POST") {
+//     console.log(`here!!!`);
+//     const fen2 = getFen();
+//     const { fen, rDepth, time } = req.body;  // Destructure FEN and color from request body
+//     console.log("Received FEN:", fen, "Depth:", rDepth, "Time:", time);
+//     console.log(fen2);
+//     // Initialize a new game instance with the FEN
+    
+//     // const game = new Chess(fen2);
+
+//     const validation = validateFen(fen);
+//     console.log(validation);
+//     if (!validateFen(fen)) {
+//       return res.status(400).json({ error: "Invalid FEN string provided." });
+//     }
+
+//     // You may need to adjust how the best move is calculated based on the color
+//     // const bestMove = await getBestMove(fen);
+//     const { bestMove, score, depth, pv } = await analyzePosition(fen, rDepth, parseInt(time));
+
+//     res.status(200).json({ bestMove, score, depth, pv });
+//   } else {
+//     console.log(`cccccccc!!!`);
+//     res.status(405).end(); // Method Not Allowed
+//   }
+// };
+
+
+
+
+
 import { Chess, validateFen } from "chess.js";
 import { getBestMove } from "../../../lib/stockfish";
 import { analyzePosition } from "../../../lib/stockfish";
@@ -20,13 +57,34 @@ export default async (req, res) => {
       return res.status(400).json({ error: "Invalid FEN string provided." });
     }
 
-    // You may need to adjust how the best move is calculated based on the color
-    // const bestMove = await getBestMove(fen);
-    const { bestMove, score, depth, pv } = await analyzePosition(fen, rDepth, parseInt(time));
+    const response = await fetch(
+      "https://us-central1-echecsai-429021.cloudfunctions.net/analyzeChessPosition",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          position: fen,
+          depth: rDepth,
+          time: parseInt(time),
+        }),
+      }
+    );
 
-    res.status(200).json({ bestMove, score, depth, pv });
+     if (!response.ok) {
+      return res
+        .status(response.status)
+        .json({ error: "Error analyzing position" });
+    }
+
+    const data = await response.json();
+
+    console.log(`data: ${data}`);
+    console.log("returned json: ", JSON.stringify(data, null, 2));
+    res.status(200).json(data);
   } else {
-    console.log(`cccccccc!!!`);
     res.status(405).end(); // Method Not Allowed
   }
 };
+
